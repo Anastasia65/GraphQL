50a8fb72e0471148ca9396cb2893191f0b8f92b1
Anastasia65 <79923782+Anastasia65@users.noreply.github.com>
2022-06-18 20:42:18 +0300

    Update HomeController.cs

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using WebApplication2.Models;
+using WebApplication2.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text.Json;
+using Newtonsoft.Json;
 
 namespace WebApplication2.Controllers
 {
@@ -8,20 +10,32 @@ namespace WebApplication2.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
         {
             _logger = logger;
+            _httpClientFactory = httpClientFactory;
         }
 
-        public IActionResult Index()
+
+        private readonly IHttpClientFactory _httpClientFactory;
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var client = _httpClientFactory.CreateClient(name: "gql");
+            var query = new { query = "query {Page(page: 1){characters(id_in: [1,2,3,4,5,6,7]){id name {full} gender dateOfBirth {year month day} age siteUrl}}}" };
+            var a = await client
+                .PostAsJsonAsync(requestUri: "https://graphql.anilist.co/", value: query);
+            var result = await a.Content.ReadFromJsonAsync<GqlRequest>();//await a.Content.ReadAsStringAsync();
+            //Deserialization(result);
+            var sh = result.data?.Page?.characters ?? new Character { };
+
+            return View(sh);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        //private void Deserialization(string result)
+        //{
+        //    var people = JsonConvert.DeserializeObject <List<Character>>(result);
+        //    return;
+        //}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
