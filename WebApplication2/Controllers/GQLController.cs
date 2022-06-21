9b66c66185a252bc5f2759c75871a719bf124f52
Anastasia65 <anst03@mail.ru>
2022-06-21 20:48:47 +0300

    final commit2

---
+++ WebApplication2/Controllers/GQLController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class GqlRequest
+    {
+        public Data data { get; set; }
+    }
+    public class Data
+    {
+        public Page Page { get; set; }
+    }
+    public class Page
+    {
+        public Character[] characters { get; set; }
+    }
+
+    public class Character
+    {
+        public int id { get; set; }
+        public Name name { get; set; }
+        public string gender { get; set; }
+        public Dateofbirth dateOfBirth { get; set; }
+        public string age { get; set; }
+        public string siteUrl { get; set; }
+        public string description { get; set; }
+    }
+
+    public class Name
+    {
+        public string full { get; set; }
+    }
+
+    public class Dateofbirth
+    {
+        public int? year { get; set; }
+        public int? month { get; set; }
+        public int? day { get; set; }
+    }
+}
